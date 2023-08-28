3100da1ae1c770c4a40daa321c807127e74be1fb
Thangdoo <142670982+Thangdoo@users.noreply.github.com>
2023-08-27 19:46:33 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+ *
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello, I'm Thang\n");
+    printf("My major is Computer Engineering\n");
+
+    return 0;
+}
